80923c60608af039e81726fab860841eb9aa5d37
Henrique <henrique.dias@gruponewway.com.br>
2025-08-24 17:02:33 -0300

    feat: implement server initialization with GraphQL endpoint

---
+++ graphql01/src/index.ts
@@ -0,0 +1,33 @@
+import express = require("express");
+import { graphqlHTTP } from 'express-graphql';
+
+import { AppDataSource } from './data-source';
+import schema, { root } from './schema';
+
+const app = express();
+
+/**
+ * Initializes the server by setting up the GraphQL endpoint and listening on a
+ * port. If the server fails to initialize, it logs an error message.
+ */
+async function main() {
+    try {
+        await AppDataSource.initialize();
+
+        app.use("/graphql", graphqlHTTP({
+            schema: schema,
+            rootValue: root,
+            graphiql: true,
+        }));
+
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => {
+            console.log(`🚀 Server running at http://localhost:${port}/graphql`);
+        });
+    } catch (error) {
+        console.error("🚨 Error initializing the server", error);
+    }
+}
+
+main();
+
